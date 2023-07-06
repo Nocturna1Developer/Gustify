2c193bc23754f6f643e739e157de94c9b2b9bf9d
Chinamay K <techchintu2017@gmail.com>
2023-07-05 18:53:50 -0700

    Created LoadingView and printed message on ios device

---
+++ Nice Weather/Nice Weather/Views/LoadingView.swift
@@ -0,0 +1,20 @@
+//
+//  LoadingView.swift
+//  Nice Weather
+//
+//  Created by m on 7/5/23.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoadingView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingView()
+    }
+}
--- Nice Weather/Nice Weather/Views/WelcomeView.swift
+++ Nice Weather/Nice Weather/Views/WelcomeView.swift
@@ -6,12 +6,21 @@
 //
 
 import SwiftUI
+import CoreLocationUI
 
 struct WelcomeView: View {
     @EnvironmentObject var locationManager: LocationManager
     var body: some View {
         VStack{
+            VStack(spacing: 20) {
+                Text("Welcome to the weather App").bold().font(.title)
+                
+                Text("Please share your current location if you want to see the weather in your current area").padding()
+            }
+            .multilineTextAlignment(.center)
+            .padding()
             
+            LocationButton()
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
